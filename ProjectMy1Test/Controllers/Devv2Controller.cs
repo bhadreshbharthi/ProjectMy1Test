fab7eb99c290e45623d6f53ee96efecff28dbd38
DashTech <DashTech@DESKTOP-Q77O0J3>
2019-03-04 23:25:25 +0530

    dev2

---
+++ ProjectMy1Test/Controllers/Devv2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectMy1Test.Controllers
+{
+    public class Devv2Controller : Controller
+    {
+        // GET: Devv2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
